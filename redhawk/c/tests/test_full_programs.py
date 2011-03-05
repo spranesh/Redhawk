c84229b4097b1d2c6536b30a20a0ab79dfe397a6
Pranesh Srinivasan <spranesh@gmail.com>
2011-03-05 15:51:25 +0530

    Added prog00[2-6].c to test_full_programs.c

--- redhawk/c/tests/test_full_programs.py
+++ redhawk/c/tests/test_full_programs.py
@@ -14,3 +14,22 @@ def TestProgram2():
   t = test_utils.SetUp("prog002.c")
   return test_utils.ConvertTree(t)
 
+def TestProgram3():
+  """ Test prog003.c """
+  t = test_utils.SetUp("prog003.c")
+  return test_utils.ConvertTree(t)
+
+def TestProgram4():
+  """ Test prog004.c """
+  t = test_utils.SetUp("prog004.c")
+  return test_utils.ConvertTree(t)
+
+def TestProgram5():
+  """ Test prog005.c """
+  t = test_utils.SetUp("prog005.c")
+  return test_utils.ConvertTree(t)
+
+def TestProgram6():
+  """ Test prog006.c """
+  t = test_utils.SetUp("prog006.c")
+  return test_utils.ConvertTree(t)
