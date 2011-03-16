a4da2fb8da8f9209f7116237c75a628d370f539b
Pranesh Srinivasan <spranesh@gmail.com>
2011-03-16 23:54:57 +0530

    Added the node.py generation files.
    _node_cfg.yaml contains the Configuration
    _node_header.py contains the header for the file.
    _node_gen.py contains the code generation from the YAML config file.

---
+++ redhawk/common/_node_gen.py
@@ -0,0 +1,185 @@
+import util
+
+import pprint
+import yaml
+
+FILES = [("_node_cfg.yaml", "_node.py", "_node_header.py")
+#       ,("_type_cfg.yaml", "_type.py", "_type_header.py")
+        ]
+
+def GetClasses(file_body):
+  """ Get a generator of classes, given the string of the body.
+      This function guarantees that each class returned has the following
+      attributes:
+        * string
+        * args
+        * children
+        * xml
+        * json
+        * dot
+        * optargs
+        * docstring
+      All attributes except the docstring is a list."""
+        
+  nodes = yaml.load(file_body).items()
+  nodes.sort()
+  for (name, c) in nodes:
+    util.AssertWithError(c.has_key('sexp'), "%s does not have string"%name)
+    util.AssertWithError(c.has_key('args'), "%s does not have args"%name)
+
+    default_fields = {'docstring' : 'Represents a %s construct'%name
+                     ,'children' : ''
+                     ,'optargs' : ''
+                     ,'super': 'Node'
+                     ,'xml' : None 
+                     ,'json': None 
+                     ,'dot':  None }
+              
+    default_fields.update(c)
+    c = default_fields
+
+    for m in "args children optargs".split():
+      if c[m] == '':
+        c[m] = []
+      else:
+        c[m] = c[m].split(", ")
+
+    # pprint.pprint((name, c))
+    yield (name, c)
+
+def WriteMethodDeclaration(c, name, args, optargs):
+  """ Write a method declaration:
+    def name(self, args, optargs):
+    in `c`"""
+  # Function Definition
+  c.Write("def %s(self"%(name))
+  for a in args:
+    c.Write(", %s"%a)
+  for a in optargs:
+    c.Write(", %s = None"%a)
+  c.Write("):")
+  c.NewLine()
+  return
+
+
+def WriteList(c, li, prefix=None):
+  """ Write a list in `c`"""
+  if prefix:
+    li2 = [prefix + x for x in li]
+  c.Write("[")
+  c.Write(", ".join(li))
+  c.Write("]")
+  return
+
+
+def WriteSExpMethod(c, name, li, args):
+  """ Write the SExp method into `c`."""
+  WriteMethodDeclaration(c, name, [], [])
+  c.Indent()
+  c.WriteLine("li = []")
+  for a in li:
+    if type(a) is str:
+      if a in args:
+        c.WriteLine("li.append(self.%s)"%(a))
+      else:
+        c.WriteLine("li.append('%s')"%(a))
+
+    elif type(a) is list:
+      if a[0][0] is '_':
+        c.WriteLine("if %s:"%(a[0][1:]))
+        c.Indent()
+        c.Write("li.append(")
+        WriteList(c, ["':%s'"%a[0][1:]] + a[1:], prefix='self.')
+        c.Write(")")
+        c.NewLine()
+        c.Dedent()
+      else:
+        c.Write("li.append(")
+        WriteList(c, a)
+        c.Write(")")
+        c.NewLine()
+  
+  c.WriteLine("return li")
+  c.Dedent()
+  c.NewLine()
+
+
+def WriteAttributeMethod(c, name, li, args):
+  """ Write the XML, JSON or Dot Methods into `c`."""
+  WriteMethodDeclaration(c, name, [], [])
+  c.Indent()
+  c.WriteLine("d = {}")
+  c.WriteLine("d['tags'] = []")
+  for x in li:
+    if x in args:
+      c.WriteLine("d[%s] = self.%s"%(x, x))
+    else:
+      c.WriteLine("d['tags'].append('%s')"%(x))
+  c.WriteLine("return d")
+  c.Dedent()
+  c.NewLine()
+  return
+
+
+def GenerateClass(name, attrs):
+  c = util.CodeGeneratorBackend()
+  c.WriteLine("class %s(%s):"%(name, attrs['super']))
+  c.Indent()
+  c.WriteLine('"""%s"""'%(attrs['docstring']))
+
+  # Write __init__
+  WriteMethodDeclaration(c, '__init__', attrs['args'], attrs['optargs'])
+  c.Indent()
+
+  args = attrs['args'] + attrs['optargs']
+  for x in args:
+    c.WriteLine("self.%s = %s"%(x, x))
+  c.WriteLine("return")
+  c.Dedent()
+  c.NewLine()
+
+
+  # Write GetChildren
+  WriteMethodDeclaration(c, "GetChildren", [], [])
+  c.Indent()
+  c.Write("return ")
+  WriteList(c, attrs['children'], prefix='self.')
+  c.NewLine()
+  c.Dedent()
+  c.NewLine()
+
+  WriteSExpMethod(c, 'GetSExp', attrs['sexp'], args)
+
+  if attrs['xml']:
+    WriteAttributeMethod(c, 'GetXMLAttributes', attrs['xml'], args)
+
+  if attrs['json']:
+    WriteAttributeMethod(c, 'GetJSONAttributes', attrs['json'], args)
+
+  if attrs['json']:
+    WriteAttributeMethod(c, 'GetDotAttributes', attrs['dot'], args)
+  c.Dedent()
+  c.NewLine()
+
+  c.GetCode()
+  return c.GetCode()
+
+
+def GetYAMLFileAsPythonCode(filename):
+  """ Return the YAML file as a string of Python Code."""
+  return "\n".join(
+      map(lambda a: GenerateClass(a[0], a[1]),
+        GetClasses(open(filename).read())))
+
+
+def Main():
+  for (ip_file, op_file, header_file) in FILES:
+    print(ip_file, op_file, header_file)
+    op = open(op_file, "w")
+    op.write(open(header_file).read())
+    op.write("\n")
+    op.write(GetYAMLFileAsPythonCode(ip_file))
+  return 0
+
+if __name__ == '__main__':
+  Main()
---
+++ redhawk/common/_node_header.py
@@ -0,0 +1,96 @@
+""" The Node Class. """
+
+import copy
+import pprint
+
+# A dictionary of allowed operators, and their arity.
+ALLOWED_OPERATORS = {
+     'UNARY_MINUS'         : ('-', 1)
+    ,'UNARY_PLUS'          : ('+', 1)
+    ,'SIZE_OR_LEN'         : ('size-or-len-of', 1)
+
+    ,'MULTIPLY'            : ('*', 2)
+    ,'ADD'                 : ('+', 2)
+    ,'MINUS'               : ('-', 2)
+    ,'DIVIDE'              : ('/', 2)
+    ,'FLOOR_DIVIDE'        : ('//', 2)
+    ,'POWER'               : ('power', 2)
+    ,'MOD'                 : ('mod', 2)
+    ,'LSHIFT'              : ('lshift', 2)
+    ,'RSHIFT'              : ('rshift', 2)
+    ,'BITWISE_OR'          : ('bitwise-or', 2)
+    ,'BITWISE_XOR'         : ('bitwise-xor', 2)
+    ,'BITWISE_AND'         : ('bitwise-and', 2)
+    ,'BITWISE_NOT'         : ('bitwise-not', 1)
+
+    ,'BOOLEAN_AND'         :('and', 2)
+    ,'BOOLEAN_OR'          : ('or', 2)
+    ,'BOOLEAN_NOT'         : ('not', 1)
+    ,'EQ'                  : ('eq', 2)
+    ,'NOT_EQ'              : ('not-eq', 2)
+    ,'IS'                  : ('is', 2)
+    ,'IN'                  : ('in', 2)
+    ,'LT'                  : ('<', 2)
+    ,'LTE'                 : ('<=', 2)
+    ,'GT'                  : ('>', 2)
+    ,'GTE'                 : ('>=', 2)
+
+    # TODO(spranesh): Should we make this an if-else?
+    ,'TERNARY_IF'          : ('ternary-if', 3) 
+
+    ,'PRE_INCREMENT'       : ('pre-increment', 1)
+    ,'POST_INCREMENT'      : ('post-increment', 1)
+    ,'PRE_DECREMENT'       : ('pre-decrement', 1)
+    ,'POST_DECREMENT'      : ('post-decrement', 1)
+    ,'ADDRESS_OF'          : ('addr-of', 1)
+    ,'POINTER_DEREFERENCE' : ('dereference-ptr', 1)
+
+    ,'ATTRIBUTE_INDEX'     : ('.', 2)
+    ,'ARROW'               : ('->', 2)
+    ,'TYPE_CAST'           : ('cast', 2)
+}
+
+
+class Node:
+  """A Parse Tree Node."""
+  def __init__(self):
+    raise NotImplementedError("Node is an Abstract Class.")
+    return
+
+  def __repr__(self):
+    return self.ToStr()
+
+  def __str__(self):
+    return self.ToStr()
+
+  def MakeCopy(self):
+    return copy.deepcopy(self)
+
+  def GetChildren(self):
+    raise NotImplementedError("Base Node Class!")
+
+  def ToStr():
+    return pprint.pformat(self.GetSExp())
+
+  def GetSExp(self):
+    raise NotImplementedError("Base Node Class!")
+
+  def GetAttributes(self):
+    """ Return the attributes of the class as a 
+    pair - (class-name, dictionary-of-attributes). """
+    d = {}
+    d['tags'] = []
+    for x in dir(self):
+      if 'a' <= x[0] <= 'z':
+        d[x] = getattr(self, x)
+    return d
+
+  def GetXMLAttributes(self):
+    return self.GetAttributes()
+
+  def GetJSONAttributes(self):
+    return self.GetAttributes()
+
+  def GetDotAttributes(self):
+    return self.GetAttributes()
+
