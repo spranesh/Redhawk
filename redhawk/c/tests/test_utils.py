8e1ad4cecdaca5b60ecc4e3737460b15fcd17b1b
Pranesh Srinivasan <spranesh@gmail.com>
2011-03-18 20:11:13 +0530

    Pickled the C Parsing. Tests are a LOT faster.

--- redhawk/c/tests/test_utils.py
+++ redhawk/c/tests/test_utils.py
@@ -2,19 +2,40 @@ import redhawk.c.c_tree_converter as C
 
 import pycparser
 
+import os.path
+import pickle as P
 import sys
 
 RELATIVE_TEST_PATH = "c/tests/c_files/"
+PICKLE_FILE = "c/tests/c_parsed.pickle"
 
 def SetUp(filename, rel_path=RELATIVE_TEST_PATH):
-  """ Parse the file using pycparser and return the parsed AST."""
   filename = rel_path + filename
+  try:
+    fp = open(PICKLE_FILE)
+    parsed_data = P.load(fp)
+    fp.close()
+  except (IOError, EOFError):
+    parsed_data = {}
+
+  basefile_name = os.path.basename(filename)
+  if parsed_data.has_key(basefile_name):
+    return parsed_data[basefile_name]
+
+  parsed_data[basefile_name] = ParseC(filename)
+  fp = open(PICKLE_FILE, "w")
+  P.dump(parsed_data, fp)
+  fp.close()
+  return parsed_data[basefile_name]
+  
+
+def ParseC(filename):
+  """ Parse the file using pycparser and return the parsed AST."""
   try:
     tree = pycparser.parse_file(filename, use_cpp = True)
   except StandardError, e:
     sys.stderr.write(str(e))
     assert(False)
-  # print open(filename).read()
   return tree
 
 
