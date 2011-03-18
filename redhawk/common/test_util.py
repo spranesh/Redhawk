5462007f3b91ff7ca2dbbb445ae2cec47ac134fc
Pranesh Srinivasan <spranesh@gmail.com>
2011-03-18 19:56:20 +0530

    Added a test_util in common/

---
+++ redhawk/common/test_util.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+import redhawk.c.tests.test_utils as CU
+
+import glob
+
+def ConvertCProgram(filename):
+  t = CU.SetUp(filename, rel_path = '')
+  return CU.ConvertTree(t)
+
+def ConvertPythonProgram(filename):
+  pass
+
+
+AST_FILES_AND_CONVERTERS = [
+    ("c/tests/c_files/*.c", ConvertCProgram)
+]
+
+def GetAllLASTs():
+  for (g, f) in AST_FILES_AND_CONVERTERS:
+    for filename in glob.glob(g):
+      yield f(filename)
+
+def TestGetAllLASTs():
+  count = 0
+  for i in GetAllLASTs():
+    count += 1
+    print "." ,
+
+  print
+  print count
+  return
+
