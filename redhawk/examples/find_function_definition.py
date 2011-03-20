66b289ca249a5193c80a9270751726096a96b601
Pranesh Srinivasan <spranesh@gmail.com>
2011-03-20 18:34:27 +0530

    1st example : Find line of a particular function definition.
    Examples directory created.
    
    The simple program finds the definition(s) of a given function in a list of
    files.

---
+++ redhawk/examples/find_function_definition.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+import redhawk.common.selector as S
+import redhawk.utils.get_ast as G
+import redhawk.utils.util as U
+
+import sys
+
+def Usage(n):
+  sys.stdout.write("%s function-name [list-of-files]\n"%(sys.argv[0]))
+  sys.exit(n)
+
+def Main():
+  if len(sys.argv) < 3:
+    Usage(1)
+  if "-h" in sys.argv:
+    Usage(0)
+
+  function_name = sys.argv[1]
+  files = sys.argv[2:]
+
+  asts = [G.GetLAst(x, pickle_file = None) for x in files]
+
+  # Ugly way of writing the selector
+  # selector = lambda x: x.GetName() == 'DefineFunction' and x.name == function_name
+
+  # Neat way of writing the selector
+  selector = S.Combine(
+      S.NodeSelector('DefineFunction'),
+      S.AttributeSelector(name = function_name))
+
+  functions_wanted = U.Concat([list(S.Select(ast, selector)) for ast in asts])
+
+  for f in functions_wanted:
+    print f.position
+  return
+
+
+if __name__ == '__main__':
+  Main()
