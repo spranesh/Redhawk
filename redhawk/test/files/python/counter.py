13622cfe639fd390c09eebb54568ab93c57e8f69
Pranesh Srinivasan <spranesh@cse.iitm.ac.in>
2011-05-11 19:17:39 +0530

    Added a counter.py in test/files/python for illustration purposes

---
+++ redhawk/test/files/python/counter.py
@@ -0,0 +1,28 @@
+def CounterClosure(init=0):
+  value = [init]
+  def Inc():
+    value[0] += 1
+    return value[0]
+  return Inc
+
+class CounterClass:
+  def __init__(self, init=0):
+    self.value = init
+
+  def Bump(self):
+    self.value += 1
+    return self.value
+
+def CounterIter(init = 0):
+  while True:
+    init += 1
+    yield init
+
+if __name__ == '__main__':
+  c1 = CounterClosure()
+  c2 = CounterClass()
+  c3 = CounterIter()
+  assert(c1() == c2.Bump() == c3.next())
+  assert(c1() == c2.Bump() == c3.next())
+  assert(c1() == c2.Bump() == c3.next())
+  
