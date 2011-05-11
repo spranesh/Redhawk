0dc5bb0e2138376621cc880c6c35ed09841bb9e6
Pranesh Srinivasan <spranesh@cse.iitm.ac.in>
2011-05-11 14:14:23 +0530

    Working setup.py! Finally!
    * Tests are still a problem to work with.

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 fp = open("README.txt")
 long_description=fp.read()
@@ -11,24 +11,49 @@ setup(
     long_description=long_description,
     author="Pranesh Srinivasan",
     author_email="spranesh@gmail.com",
-    license="BSD Two Clause License",
+    license="The BSD 2-Clause License",
     url="http://pypi.python.org/pypi/redhawk/",
 
-    packages=['redhawk'
-             ,'redhawk.examples'
-             ,'redhawk.c'
-             ,'redhawk.common'
-             ,'redhawk.common.readers'
-             ,'redhawk.common.writers'
-             ,'redhawk.python'
-             ,'redhawk.utils'],
+    # packages = find_packages()
+    packages=[
+      'redhawk',
+      'redhawk.examples',
+      'redhawk.c',
+      'redhawk.c.utils',
+      'redhawk.common',
+      'redhawk.common.readers',
+      'redhawk.common.writers',
+      'redhawk.python',
+      'redhawk.python.utils',
+      'redhawk.utils',
+      'redhawk.test'],
 
-    install_requires = ['PyYAML>=3.09'
-                      ,'nose>=0.11'
-                      ,'pycparser>=2.02'
-                      # ,'pygraphviz>=0.99' # Optional Dependency
-                      ],
+    package_data = {
+      'redhawk.utils': ['fake_libc_include/*'],
+      'redhawk.test': ['files/python/*', 'files/c/*', 'files/dot/*'],
+      'redhawk.common': ['*.yaml']
+      },
 
-    scripts=['bin/redhawk_prompt'
-            ,'bin/redhawk']
+    install_requires = [
+      'PyYAML>=3.09',
+      'nose>=0.11',
+      # ,'pygraphviz>=0.99' # Optional Dependency
+      'pycparser>=2.02' ],
+
+    classifiers = [
+      "Development Status :: 3 - Alpha",
+      "Environment :: Console",
+      "Environment :: Plugins",
+      "Intended Audience :: Developers",
+      "License :: OSI Approved :: BSD License",
+      "Operating System :: POSIX :: Linux",
+      "Programming Language :: C",
+      "Programming Language :: Python :: 2.6",
+      "Programming Language :: Python :: 2.7",
+      "Topic :: Software Development",
+      "Topic :: Utilities"],
+
+    scripts=[
+      'bin/redhawk_prompt',
+      'bin/redhawk']
 )
