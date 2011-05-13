eb055e18d357470184073524bd0117ed865f76d1
Pranesh Srinivasan <spranesh@cse.iitm.ac.in>
2011-05-13 14:18:59 +0530

    Improved bin/start_simple_bash_with_redhawk_in_pythonpath.sh

---
+++ bin/start_simple_bash_with_redhawk_in_pythonpath.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+RELDIR=../redhawk
+
+
+SCRIPT=`readlink -f $0`
+SCRIPTPATH=`dirname $SCRIPT`
+echo "[0] Script Directory: $SCRIPTPATH"
+REDHAWK=`readlink -f $SCRIPTPATH/$RELDIR`
+echo "[1] Adding $REDHAWK to path."
+
+export PYTHONPATH="$REDHAWK:$PYTHONPATH"
+echo "[2] New Python Path is $PYTHONPATH"
+
+echo "[3] Starting bash"
+echo "[4] Run nosetests from the $REDHAWK directory."
+
+bash --norc --noprofile
+
